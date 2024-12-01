fa4bb1260322460893f21d04465829a8ef98ace0
Sudhakhar-Katta <satyakatta11@gmail.com>
2024-11-30 18:03:07 -0800

    I have given player the ability to choose their own name aswell as their own game piece

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct{
+    Board board;
+    int curr_player_turn;
+}Game;
+
+
 typedef struct{
     char** contents;
     int dimensions;
     char blank_char;
 }Board; 
 
+typedef struct{
+    char* name;
+    char piece;
+}Player;
+
 /*
 The following code about shows what content is need to create a board which would be the dimensiosn and the n*n grid and a blank character to emulate the empty box in a 
 normal game of tic tac toe
 */
-
-typedef struct 
-{
-    /* data */
-}Game;
-
 
 Board create_board(int dimensions,char blank_char){
     Board board;
@@ -34,14 +43,73 @@ and what they want their blank character to be to simulate the space in a regula
 function saying to return something that is Board
 */
 
+int pick_who_goes_first(int num_players){
+    return rand() % num_players;    
+}
+char* get_player_name(Player* players_in_game, int len){
 
-Game setup_game(){
+    char* name= NULL;
+    do{
+        if(name != NULL){
+            free(name);
+            name=NULL;
+        }
+
+        printf("Enter your name: ");
+        name=get_any_len_string();
+        if(!is_valid_format(1,1)){
+            printf("Your name can only be 1 word long.\n");
+            continue;
+        }else if(!is_player_in_game(name,players_in_game,len)){
+            printf("%s is already in the game. Choose another name.\n");
+        }else{
+            break;
+        }
+    }while(true);
+    return name;
+}   
+
+char* get_player_piece(Player* players_in_game,int len){
+    char piece;
+    
+    while(true){
+        piece=get_valid_char("Please choose your piece: ");
+        if(is_piece_is_already_chosen(piece,players_in_game,len)){
+            printf("You can't choose %c because another player has already choosen it\n");
+        }else{
+            return piece;
+        }
+    }
+}
+
+
+Player* players_pick_names_and_pieces(int num_players){
+    Player* players= (Player*)calloc(num_players,sizeof(Player));
+    for(int i=0;i<num_players;++i){
+        players[i].name=get_player_name();
+        players[i].piece=get_player_piece();
+    }
+}
+
+Game setup_game(int dimensions, char blank_char){
     Game game;
-    create_board();
-    pick_who_goes_first();
+    game.board=create_board(dimensions,blank_char);
+    game.curr_player_turn=pick_who_goes_first(2);
     players_pick_pieces();
     return game;
 }
+
+bool is_game_over(){
+    return someone_won() ||  tie();
+}
+
+void take_turn(Game *game){
+    Player* curr_player=get_curr_player();
+    Move move= get_move();
+    game ->board->contents[move.row][move.col]= curr_player->piece;
+}
+
+
 
 void play_game(Game *game){  // THis is a game pointer because we actually want the game not a copy
     while(!is_game_over()){
